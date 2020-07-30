6b10ac5e2b70ec48ae8ca4e8724d758d356e23ca
Srinu Pandra <67537872+srinupandra1@users.noreply.github.com>
2020-07-30 13:21:03 +0530

    Add files via upload

---
+++ letterchange.c
@@ -0,0 +1,14 @@
+//TO change the letter from the string
+
+#include <stdio.h>
+int main()
+{
+	char Text[] = "ravi";
+	printf("%s\n",Text);
+	 Text[2] = 'n';
+	 printf("%s\n",Text);
+	 return 0;
+}
+
+//     output :- ravi
+//               rani
