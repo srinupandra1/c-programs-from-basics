ac1181e4b861a01a1bcca10a71403d8a2c20eae5
Srinu Pandra <67537872+srinupandra1@users.noreply.github.com>
2020-08-04 15:43:31 +0530

    Add files via upload

---
+++ selectword.c
@@ -0,0 +1,15 @@
+// To select the word from the array
+
+
+
+#include<stdio.h>
+int main()
+{
+char *student[] = {"srinu","yuvi","raga","sam"};
+printf("The best student is %s\n", student[1]);
+return 0;
+}
+
+
+
+//output :   The best student is yuvi
